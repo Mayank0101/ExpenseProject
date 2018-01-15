ef938759ac84c0fba9c260f2cfe0b9c5b34b80a5
Mayank Shrivastava <mks01011986@gmail.com>
2018-01-15 12:59:28 -0600

    adding controller

--- MyHomeExpensesV1/src/main/java/com/homeexpense/controller/ExpensesController.java
+++ MyHomeExpensesV1/src/main/java/com/homeexpense/controller/ExpensesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.homeexpense.business.ExpensesBusiness;
@@ -32,6 +33,24 @@ public class ExpensesController {
 		expenseBiz.addExpenses(expenses);
 		return true;
 	}
+	
+	@RequestMapping(value="/addExpenses",method=RequestMethod.GET)
+	public boolean addExpensesGet(@RequestParam String startDate,
+			@RequestParam String retailer,@RequestParam String productName,@RequestParam String size,@RequestParam String price) {
+		Expenses expenses = new Expenses();
+		System.out.println(expenses.getEndDate());
+		expenseBiz.addExpenses(expenses);
+		return true;
+	}
+	
+	@RequestMapping(value="/addExpenses1",method=RequestMethod.GET)
+	public boolean addExpensesGet1(@RequestBody Expenses expenses) {
+		System.out.println(expenses.getEndDate());
+		expenseBiz.addExpenses(expenses);
+		return true;
+	}
+	
+	
 	@GetMapping("/removeExpenses")
 	public boolean removeExpenses(@PathVariable String studentId) {
 		return false;
