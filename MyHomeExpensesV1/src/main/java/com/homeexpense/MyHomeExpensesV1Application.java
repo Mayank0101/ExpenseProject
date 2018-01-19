de96d72e9944f1c225e491625883c790881c3c84
Mayank Shrivastava <mashr3@wal-mart.com>
2018-01-19 13:37:12 -0600

    Adding Cors filter

--- MyHomeExpensesV1/src/main/java/com/homeexpense/MyHomeExpensesV1Application.java
+++ MyHomeExpensesV1/src/main/java/com/homeexpense/MyHomeExpensesV1Application.java
@@ -3,7 +3,11 @@ package com.homeexpense;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @Configuration
@@ -13,4 +17,15 @@ public class MyHomeExpensesV1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(MyHomeExpensesV1Application.class, args);
 	}
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")/*.allowedOrigins("http://localhost:9000")*/;
+            }
+        };
+    }
 }
--- MyHomeExpensesV1/src/main/java/com/homeexpense/utils/ApplicationUtil.java
+++ MyHomeExpensesV1/src/main/java/com/homeexpense/utils/ApplicationUtil.java
@@ -1,5 +1,11 @@
 package com.homeexpense.utils;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 public class ApplicationUtil {
+
 
 }
